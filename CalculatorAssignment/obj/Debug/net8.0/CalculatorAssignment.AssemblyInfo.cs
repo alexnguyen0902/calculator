304a5dac8ebe3606e8721816c55f4b4f1d7210d8
alexnguyen0902 <alexhoangbach@gmail.com>
2024-02-25 18:58:00 +1100

    test

--- CalculatorAssignment/obj/Debug/net8.0/CalculatorAssignment.AssemblyInfo.cs
+++ CalculatorAssignment/obj/Debug/net8.0/CalculatorAssignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorAssignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41af4ec85271df2a0dc3183c5bdb81106eb8c52d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c63c49666d49f93f798e46385b0172a5278c34ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorAssignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorAssignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
